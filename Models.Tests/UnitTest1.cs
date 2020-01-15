baaeda939ab70385cea81ace0e77d101f97efffc
Hammond Soares <hammond.soares@gmail.com>
2020-01-14 22:00:32 -0500

    First commit.

---
+++ Models.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Models.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
